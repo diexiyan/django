ad2ca7c36a1db5e7a2032636ef5a8ad8da876570
bh <3020498526@qq.com>
2019-04-16 16:29:32 +0800

    添加了数据

--- demoa/booktest/admin.py
+++ demoa/booktest/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BookInfo,Hero
 
 # Register your models here.
+admin.site.register(Hero)
+admin.site.register(BookInfo)
--- demoa/booktest/models.py
+++ demoa/booktest/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class BookInfo(models.Model):
+    bname = models.CharField(max_length=20)
+    bpub_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.bname
+
+class Hero(models.Model):
+    hname = models.CharField(max_length=20)
+    bsex = models.BooleanField()
+    bfrom_bid = models.ForeignKey('BookInfo', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.hname
+
+
+
